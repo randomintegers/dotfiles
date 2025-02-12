9147e15f0f2a12a96598e98a056bcc15ad6ec392
randomintegers <raahimazfar@gmail.com>
2025-02-12 19:01:52 +0300

    Add files via upload
    added dots

---
+++ cmus/lib.pl
@@ -0,0 +1,87 @@
+/home/raahimazfar/Music/halal/31.mp3
+/home/raahimazfar/Music/lofi/Laid back lutes for leading the charge.mp3
+/home/raahimazfar/Music/lofi/Soothing synths for mining blocks.mp3
+/home/raahimazfar/Music/lofi/Tranquil beats for Tower runs.mp3
+/home/raahimazfar/Music/random/gangnam.mp4
+/home/raahimazfar/Music/rush/rush E.mp3
+/home/raahimazfar/Music/tricky_trials/featherfall.mp3
+/home/raahimazfar/Music/tricky_trials/precipice.mp3
+/home/raahimazfar/Music/tricky_trials/puzzlebox.mp3
+/home/raahimazfar/Music/tricky_trials/watcher.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Beginning.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Cat.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Chris.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Clark.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Danny.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Dog.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Door.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Droopy Likes Your Face.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Équinoxe.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Excuse.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Haggstrom.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Mice On Venus.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Moog City.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Subwoofer Lullaby.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Thirteen.mp3
+/home/raahimazfar/Music/minecraft_volume_alpha/Wet Hands.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Alpha.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Aria Math.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Biome Fest.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Blind Spots.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Chirp.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Eleven.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Floating Trees.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Haunt Muskie.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Intro.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Kyoto.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Mall.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Mellohi.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Stal.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Strad.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Taswell.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Wait.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Ward.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Ki.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Dead Voxel.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Flake.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Moog City 2.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Concrete Halls.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Mutation.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Warmth.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Ballad of the Cats.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Beginning 2.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Dreiton.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/The End.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Blocks.mp3
+/home/raahimazfar/Music/Minecraf-Volume_Beta/Far.mp3
+/home/raahimazfar/Music/geometry dash/Clubstep.mp3
+/home/raahimazfar/Music/geometry dash/Electrodynamix.mp3
+/home/raahimazfar/Music/geometry dash/TheoryOfEverything.mp3
+/home/raahimazfar/Music/geometry dash/BaseAfterBase.mp3
+/home/raahimazfar/Music/geometry dash/CantLetGo.mp3
+/home/raahimazfar/Music/geometry dash/DryOut.mp3
+/home/raahimazfar/Music/geometry dash/BackOnTrack.mp3
+/home/raahimazfar/Music/geometry dash/Cycles.mp3
+/home/raahimazfar/Music/geometry dash/xStep.mp3
+/home/raahimazfar/Music/geometry dash/Deadlocked.mp3
+/home/raahimazfar/Music/geometry dash/StereoMadness.mp3
+/home/raahimazfar/Music/tricky_trials/komorebi.mp3
+/home/raahimazfar/Music/tricky_trials/pokopoko.mp3
+/home/raahimazfar/Music/tricky_trials/yakusoku.mp3
+/home/raahimazfar/Music/nether/chrysopoeia.mp3
+/home/raahimazfar/Music/nether/pigstep_mono.mp3
+/home/raahimazfar/Music/nether/pigstep_stereo.mp3
+/home/raahimazfar/Music/nether/rubedo.mp3
+/home/raahimazfar/Music/nether/so_below.mp3
+/home/raahimazfar/Music/tricky_trials/creator.mp3
+/home/raahimazfar/Music/tricky_trials/creator_music_box.mp3
+/home/raahimazfar/Music/tricky_trials/deeper.mp3
+/home/raahimazfar/Music/tricky_trials/unknown.mp3
+/home/raahimazfar/Music/tricky_trials/endless.mp3
+/home/raahimazfar/Music/geometry dash/Polargeist.mp3
+/home/raahimazfar/Music/geometry dash/BlastProcessing.mp3
+/home/raahimazfar/Music/geometry dash/TimeMachine.mp3
+/home/raahimazfar/Music/geometry dash/Jumper.mp3
+/home/raahimazfar/Music/geometry dash/Clutterfunk.mp3
+/home/raahimazfar/Music/geometry dash/Electroman.mp3
+/home/raahimazfar/Music/geometry dash/HexagonForce.mp3
